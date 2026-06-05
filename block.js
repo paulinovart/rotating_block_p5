a616d4ec0c375e379ffaf7ed65cbf5611e0a638a
paulinovart <pauline.dierick@gmail.com>
2026-06-05 10:25:15 +0200

    initial commit

---
+++ block.js
@@ -0,0 +1,31 @@
+class Block {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.angle = 0;
+    }
+
+    display() {
+        push();
+        translate(this.x, this.y); // set origin point, to rotate from  the right point => rn origin = center
+
+        rotate(this.angle);
+        rect(0, 0, size, size ); // position x du coin gauche, position y du coin gauche, longueur, hauteur
+        pop();
+    }
+
+    move(){
+        
+        let distance = dist(mouseX, mouseY,this.x, this.y );
+        if (distance < distMouse){    
+            this.angle += 1; // vu que la fonction est run 30 frames par secondes, ça touuuuuurne
+        } 
+        else{
+        if(this.angle > 0 && this.angle < 90 ){
+            this.angle += 1; 
+        } else {
+            this.angle = 0;
+        }
+    }
+}
+}
---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+let distMouse = 50;
+//let b;
+let cols; let rows; let size = 50;
+let blocks = [];
+
+function setup() {
+    createCanvas(800, 800);
+    rectMode(CENTER);
+    angleMode(DEGREES);
+    cols = width/size;
+    rows = height/size;
+
+    for (let i=0; i<cols; i++){
+        blocks[i] = [];
+        for (j=0; j<rows; j++){
+            blocks[i][j] = new Block(size/2 + i*size, size/2 + j*size); //pour set la position en fonction du nombre de carrés déjà faits
+        }
+    }
+
+
+}
+
+function draw(){
+background (220)
+    for (let i=0; i<cols; i++){
+
+        for (j=0; j<rows; j++){
+            blocks[i] [j].move();
+            blocks[i] [j].display();
+        }
+    }
+
+
+
+
+   // background(220);
+   //  b.move();
+   // b.display();
+
+
+
+    //console.log(distance)
+   
+
+}
